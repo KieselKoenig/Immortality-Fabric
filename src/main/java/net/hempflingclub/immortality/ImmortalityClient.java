6bc67e5b81fff4b73ac16bcdba9a951668e27a95
Hempflingclub <29497373+Hempflingclub@users.noreply.github.com>
2022-10-03 01:56:26 +0200

    Initial commit

---
+++ src/main/java/net/hempflingclub/immortality/ImmortalityClient.java
@@ -0,0 +1,11 @@
+package net.hempflingclub.immortality;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ImmortalityClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
