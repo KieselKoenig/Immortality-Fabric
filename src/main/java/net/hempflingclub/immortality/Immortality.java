e4be2cf4d99b1c7778d112752507be688f945d1b
Hempflingclub <29497373+Hempflingclub@users.noreply.github.com>
2022-10-03 02:49:18 +0200

    Test

--- src/main/java/net/hempflingclub/immortality/Immortality.java
+++ src/main/java/net/hempflingclub/immortality/Immortality.java
@@ -11,5 +11,6 @@ public class Immortality implements ModInitializer {
     @Override
     public void onInitialize() {
         net.hempflingclub.immortality.item.UsableItems.registerModItems();
+        //testing Push
     }
 }
